f92e1e33a2edc584ba11735acc3825b352d53e82
ifnuu01 <ifnuu01@gmail.com>
2025-05-20 22:08:00 +0800

    feat : menambahkan api booking tool dan bookin room

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\RoomBookingController;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\ToolBookingController;
 use App\Http\Controllers\ToolController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -25,3 +27,7 @@ Route::apiResource('tools', ToolController::class);
 
 Route::post('rooms/bulk-destroy', [RoomController::class, 'bulkDestroy']);
 Route::post('tools/bulk-destroy', [ToolController::class, 'bulkDestroy']);
+Route::post('rooms/booking-rooms', [RoomBookingController::class, 'store']);
+Route::delete('rooms/booking-rooms/{id}', [RoomBookingController::class, 'destroy']);
+Route::post('tools/booking-tools', [ToolBookingController::class, 'store']);
+Route::delete('tools/booking-tools/{id}', [ToolBookingController::class, 'destroy']);
